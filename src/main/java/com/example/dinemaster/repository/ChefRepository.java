a172f14912c7ba2babbf18f9fabfa43e4d3e2afd
Mounika <bsmounika15@gmail.com>
2024-07-03 06:19:07 +0000

    solution

---
+++ src/main/java/com/example/dinemaster/repository/ChefRepository.java
@@ -0,0 +1,20 @@
+package com.example.dinemaster.repository;
+
+import com.example.dinemaster.model.Chef;
+import com.example.dinemaster.model.Restaurant;
+
+import java.util.ArrayList;
+
+public interface ChefRepository {
+    ArrayList<Chef> getChefs();
+
+    Chef getChefById(int chefId);
+
+    Chef addChef(Chef chef);
+
+    Chef updateChef(int chefId, Chef chef);
+
+    void deleteChef(int chefId);
+
+    Restuarant getChefRestuarant(int chefId);
+}
